fe66fc5d53a167ad82e3814a66d4b78bd7830fb8
Bekbolsun Akimov <akimovbekbolsun332@gmail.com>
2026-01-04 18:23:30 +0600

    Add fonts outfit

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello, I'm first time here!</h1>
+      <Button
+      variant="outline"
+      >Subscribe</Button>
     </div>
   );
 }
