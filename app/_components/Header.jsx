168b04051d67eb969fb3fc4585eb204dd516fa55
Bekbolsun Akimov <akimovbekbolsun332@gmail.com>
2026-01-16 21:50:46 +0600

    Fix danjerous errors

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
-import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 
 const Header = () => {
   return (
-    <div className='p-5'>
-      <Image src={'/logo.svg'}
-      alt='logo'
-      width={160}
-      height={100}
-      />
+    <div className='p-5 flex justify-between items-center border shadow-sm'>
+    
       <Button>Get Started</Button>
     </div>
   )
