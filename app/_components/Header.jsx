bf55c4fde2df68d087cef45aafd161108d28b909
Bekbolsun Akimov <akimovbekbolsun332@gmail.com>
2026-01-24 23:08:23 +0600

    Fix some errors

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 
 const Header = () => {
   return (
     <div className='p-5 flex justify-between items-center border shadow-sm'>
-    <Image src={'/logo.png'}
-    alt='logo'
-    width={160}
-    height={100}/>
-    <Button>Get Started</Button>
+      <Image 
+        src={'/logo.png'}
+        alt='logo'
+        width={160}
+        height={100}
+        priority
+      />
+      <Link href="/sign-up">
+        <Button>Get Started</Button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
