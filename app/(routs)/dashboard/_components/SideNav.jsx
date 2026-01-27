89dcdc93ee84dc22f540cbf349bf883b27378d07
Bekbolsun Akimov <akimovbekbolsun332@gmail.com>
2026-01-28 04:39:40 +0600

    Add menu sidebar

--- app/(routs)/dashboard/_components/SideNav.jsx
+++ app/(routs)/dashboard/_components/SideNav.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
 import Image from 'next/image';
+import {LayoutGrid, PiggyBank, ReceiptText, ShieldCheck} from 'lucide-react';
 
 function SideNav () {
+    const menuList = [
+        {
+            id: 1,
+            name: 'Dashboard',
+            icon: LayoutGrid
+        },
+        {
+            id: 2,
+            name: 'Budgets',
+            icon: PiggyBank
+        },
+        {
+            id: 3,
+            name: 'Expenses',
+            icon: ReceiptText
+        },
+        {
+            id: 4,
+            name: 'Upgrade',
+            icon: ShieldCheck
+        }
+        
+    ]
     return (
-        <div className='h-screen p-5'>
+        <div className='h-screen p-5 border shadow-sm'>
             <Image src={'/logo.png'} alt='logo' width={160} height={100} />
+            <div>
+                {menuList.map((menu, index)=> (
+                    <h2>
+                        <menu.icon/>
+                        {menu.name}
+                    </h2>
+                ))}
+            </div>
         </div>
 
     )
--- app/(routs)/layout.jsx
+++ app/(routs)/layout.jsx
@@ -4,7 +4,7 @@ import SideNav from './dashboard/_components/SideNav';
 export default function DashboardLayout({ children }) {
     return (
         <div>
-            <div className='fixed md:w-64 hidden md:block bg-red-200'>
+            <div className='fixed md:w-64 hidden md:block'>
                 <SideNav/>
             </div>
             <div className='md:ml-64 bg-green-400'>
